ccaac58a544b508cbbda162007cac9b979508cb8
Pwirth <wirth@hallowelt.biz>
2018-02-27 12:35:42 +0000

    BSAuthors: Changed settings paths from 2 to 3 sections / Fixes
    => Requires: I4c645fff9932e7235bcd82513b30b36414f79378
    
    Change-Id: I19b39b8ef1724b8ff5b62481efc71a5ae545cd25

--- src/ConfigDefinition/AuthorsLimit.php
+++ src/ConfigDefinition/AuthorsLimit.php
@@ -2,9 +2,18 @@
 
 namespace BlueSpice\Authors\ConfigDefinition;
 
-use BlueSpice\ConfigDefinition\ArraySetting;
+use BlueSpice\ConfigDefinition\IntSetting;
 
-class AuthorsLimit extends ArraySetting {
+class AuthorsLimit extends IntSetting {
+
+	public function getPaths() {
+		return [
+			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_PERSONALISATION . '/BlueSpiceAuthors',
+			static::MAIN_PATH_EXTENSION . '/BlueSpiceAuthors/' . static::FEATURE_PERSONALISATION ,
+			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_FREE . '/BlueSpiceAuthors',
+		];
+	}
+
 	public function getLabelMessageKey() {
 		return 'bs-authors-pref-limit';
 	}
--- src/ConfigDefinition/AuthorsShow.php
+++ src/ConfigDefinition/AuthorsShow.php
@@ -5,6 +5,15 @@ namespace BlueSpice\Authors\ConfigDefinition;
 use BlueSpice\ConfigDefinition\BooleanSetting;
 
 class AuthorsShow extends BooleanSetting {
+
+	public function getPaths() {
+		return [
+			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_DATA_ANALYSIS . '/BlueSpiceAuthors',
+			static::MAIN_PATH_EXTENSION . '/BlueSpiceAuthors/' . static::FEATURE_DATA_ANALYSIS ,
+			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_FREE . '/BlueSpiceAuthors',
+		];
+	}
+
 	public function getLabelMessageKey() {
 		return 'bs-authors-pref-show';
 	}
