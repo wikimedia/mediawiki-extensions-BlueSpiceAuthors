33b0d7abe9d40ed3fcc438b6205567e5c1961c55
Pwirth <wirth@hallowelt.biz>
2018-03-13 17:22:41 +0000

    BSAuthors: Removed usage of deprecated BSUserMiniProfile
    Change-Id: Idead3050d4be625b668493c11ade186f46bf7752

--- views/view.AuthorsUserPageProfileImageSetting.php
+++ views/view.AuthorsUserPageProfileImageSetting.php
@@ -24,19 +24,7 @@ class ViewAuthorsUserPageProfileImageSetting extends ViewBaseElement {
 	/*
 	 * @var User $oUser The User object of the current user.
 	 */
-	protected $oUser                = null;
-	/*
-	 * @var string $sImagePath Path to the current user image.
-	 */
-	protected $sImagePath           = '';
-	/*
-	 * @var string $sUserDisplayName Display name of the current user.
-	 */
-	protected $sUserDisplayName     = '';
-	/*
-	 * @var string $sImageUploadPath URL of upload script for changing the current user image.
-	 */
-	protected $sImageUploadPath     = '';
+	protected $oUser = null;
 
 	/**
 	 * This method actually generates the output
@@ -44,33 +32,25 @@ class ViewAuthorsUserPageProfileImageSetting extends ViewBaseElement {
 	 * @return string HTML output
 	 */
 	public function  execute( $params = false ) {
-		$this->initFields();
-		$params = [
-			\BlueSpice\DynamicFileDispatcher\Params::MODULE => 'userprofileimage',
-			'username' =>  $this->oUser->getName(),
-		];
-		$dfdUrlBuilder = \MediaWiki\MediaWikiServices::getInstance()->getService(
-			'BSDynamicFileDispatcherUrlBuilder'
-		);
-		$this->sImagePath = $dfdUrlBuilder->build(
-			new \BlueSpice\DynamicFileDispatcher\Params( $params )
-		);
 
-		Hooks::run( 'BsAuthorPageProfileImageAfterInitFields', array( $this, $this->oUser ) );
-
-		$aOut = array();
-		$aOut[] = '<div id="bs-authors-imageform" class="bs-userpagesettings-item">';
-		$aOut[] = $this->renderLink(
-			array(
-				'href'   => htmlspecialchars( $this->sImageUploadPath ),
-				'title'  => wfMessage( 'bs-authors-profileimage-change' )->plain()
-			),
-			'<img src="'.$this->sImagePath.'" alt="'.$this->sUserDisplayName.'" width="64" title="'.wfMessage( 'bs-authors-profileimage' )->plain().'" />'.
-			'<div class="bs-user-label">'.wfMessage( 'bs-authors-profileimage-change' )->plain().'</div>'
+		$factory = \BlueSpice\Services::getInstance()->getBSRendererFactory();
+		$renderer = $factory->get( 'userimage', new \BlueSpice\Renderer\Params([
+			\BlueSpice\Renderer\UserImage::PARAM_USER => $this->oUser,
+			\BlueSpice\Renderer\UserImage::PARAM_WIDTH => 64,
+			\BlueSpice\Renderer\UserImage::PARAM_HEIGHT => 64,
+		]));
+		$out = \Html::openElement( 'div', [
+			'id' => 'bs-authors-imageform',
+			'class' => 'bs-userpagesettings-item',
+		]);
+		$out .= $renderer->render();
+		$out .= \Html::element(
+			'div',
+			[ 'class' => 'bs-user-label' ],
+			wfMessage( 'bs-authors-profileimage-change' )->plain()
 		);
-		$aOut[] = '</div>';
-
-		return implode( "\n", $aOut );
+		$out .= \Html::closeElement( 'div' );
+		return $out;
 	}
 
 	/**
@@ -79,60 +59,5 @@ class ViewAuthorsUserPageProfileImageSetting extends ViewBaseElement {
 	 */
 	public function setCurrentUser( $oUser ) {
 		$this->oUser = $oUser;
-	}
-
-	public function setImagePath( $sImagePath ) {
-		$this->sImagePath = $sImagePath;
-	}
-
-	public function getImagePath() {
-		return $this->sImagePath;
-	}
-
-	/**
-	 * Initializes required fields.
-	 */
-	private function initFields() {
-		if ( $this->oUser === null ) throw new BsException( __METHOD__.' - No user specified.' );
-
-		$this->sUserDisplayName = BsCore::getInstance()->getUserDisplayName( $this->oUser );
-		$sUserImage             = $this->oUser->getOption( 'MW::UserImage', '' ); //BsConfig::get() won't work on first call
-
-		//Is it a URL? Some external image?
-		$aParsedUrl = parse_url( $sUserImage );
-		if ( !empty($sUserImage) && ($sUserImage{0} == '/' || isset( $aParsedUrl['scheme'] )) ) {
-			$this->sImageUploadPath = SpecialPage::getTitleFor( 'Preferences' )->getLinkUrl();
-
-			$aPathInfo = pathinfo( $aParsedUrl['path'] );
-			$aFileExtWhitelist = array( 'gif', 'jpg', 'jpeg', 'png' );
-			$this->sImagePath  = $aParsedUrl['scheme'].'://'.$aParsedUrl['host'].$aParsedUrl['path'];
-
-			if ( !in_array( strtolower( $aPathInfo['extension'] ), $aFileExtWhitelist ) ) {
-				$this->sImagePath = BsConfig::get( 'MW::AnonUserImage' );
-			}
-			return;
-		}
-
-		$oUserImageFile = RepoGroup::singleton()->getLocalRepo()->newFile( $sUserImage );
-		if ( $oUserImageFile ) {
-			$UserImageArticle       = new ImagePage( $oUserImageFile->getTitle() );
-			$this->sImageUploadPath = $UserImageArticle->getUploadUrl();
-
-			if ( $oUserImageFile->exists() === false ) {
-				$this->sImagePath = BsConfig::get( 'MW::DefaultUserImage' );
-			}
-			else {
-				$oUserThumbnail = $oUserImageFile->transform( array( 'width' => 64, 'height' => 64 ) );
-				if ( $oUserThumbnail !== false ) {
-					$this->sImagePath = $oUserThumbnail->getUrl();
-				}
-				else {
-					$this->sImagePath = $oUserImageFile->getUrl();
-				}
-			}
-		}
-		else {
-			$this->sImagePath = BsConfig::get( 'MW::DefaultUserImage' );
-		}
 	}
 }
