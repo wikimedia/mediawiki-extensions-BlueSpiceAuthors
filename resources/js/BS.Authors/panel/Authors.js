9380ffe8342e96391c95a21e5e9638d46783bbfc
Dejan Savuljesku <d.savuljesku@gmail.com>
2018-07-02 12:54:08 +0200

    Add authors to ArticleInfo flyout
    Change-Id: I52d9ead1a93bef7dd22b53f5dd2609a7093c1e7c
    ERM: #9944

---
+++ resources/js/BS.Authors/panel/Authors.js
@@ -0,0 +1,39 @@
+Ext.define( 'BS.Authors.panel.Authors', {
+	extend: 'Ext.Panel',
+	cls: 'bs-authors-flyout-authors',
+	htmlForSitetools: [],
+	title: mw.message( 'bs-authors-flyout-title' ).plain(),
+	initComponent: function () {
+		var editors = this.htmlForSitetools['editors'];
+
+		var html = '<div class="flyout-authors-body">';
+
+		if( 'originator' in this.htmlForSitetools ) {
+			html += '<div class="originator">'
+				+ this.htmlForSitetools['originator'] + '</div>';
+
+		}
+
+		if( editors.length > 0 ) {
+			html += '<div class="authors">';
+
+			editors.forEach( function( value, index, array ){
+				html += value;
+			});
+
+			html += '</div>';
+
+			if( this.htmlForSitetools['more'] === true ) {
+				html += '<div class="authors-more">'
+					+ '<a href="' + mw.util.getUrl( this, { 'action': 'history' } ) + '">'
+					+ '<i class="bs-authors-more-icon"></i>'
+					+ '</a></div>';
+			}
+		}
+		html += '</div>';
+
+		this.html = html;
+
+		this.callParent( arguments );
+	}
+})
---
+++ resources/js/bluespice.authors.flyout.js
@@ -0,0 +1,25 @@
+Ext.onReady( function() {
+	Ext.Loader.setPath(
+		'BS.Authors',
+		bs.em.paths.get( 'BlueSpiceAuthors' ) + '/resources/js/BS.Authors'
+	);
+});
+
+(function( mw, $, bs, undefined ) {
+	bs.util.registerNamespace( 'bs.authors.flyout' );
+
+	bs.authors.flyout.makeItems = function() {
+		if( mw.config.get( 'bsgAuthorsSitetools' ) !== null ) {
+			return {
+				centerRight: [
+					Ext.create( 'BS.Authors.panel.Authors', {
+						htmlForSitetools: mw.config.get( 'bsgAuthorsSitetools' )
+					} )
+				]
+			}
+		}
+
+		return {};
+	};
+
+})( mediaWiki, jQuery, blueSpice );
