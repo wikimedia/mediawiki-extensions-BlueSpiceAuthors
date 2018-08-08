6c194d45087fc62c18e4210bc18b374071d715fe
Peter Boehm <boehm@hallowelt.com>
2018-08-08 11:25:05 +0200

    Refactoring Hooks of BlueSpiceAuthors
    introduced Extension.php, removed unused Hooks and Views.
    Change-Id: I1a221a3869cf8264230b852d320fab37ea20df73

--- src/Hook/BeforePageDisplay/FetchAuthors.php
+++ src/Hook/BeforePageDisplay/FetchAuthors.php
@@ -8,11 +8,34 @@ use BlueSpice\DynamicFileDispatcher\Params;
 use BlueSpice\DynamicFileDispatcher\UserProfileImage;
 
 class FetchAuthors extends BeforePageDisplay {
-	protected function doProcess() {
 
-		if( \Authors::checkContext( $this->out ) === false ) {
+	protected function skipProcessing() {
+		$config = $this->getConfig();
+		if ( $config->get( 'AuthorsShow' ) === false ) {
 			return true;
 		}
+		$title = $this->out->getTitle();
+		if ( !$title->exists() || !$title->userCan( 'read' ) ) {
+			return true;
+		}
+
+		if ( $this->out->getRequest()->getVal( 'action', 'view' ) != 'view' ) {
+			return true;
+		}
+
+		if ( in_array( $title->getNamespace(), array( NS_SPECIAL, NS_CATEGORY, NS_FILE ) ) ) {
+			return true;
+		}
+
+		// Do not display if __NOAUTHORS__ keyword is found
+		$noAuthors = \BsArticleHelper::getInstance( $title )->getPageProp( 'bs_noauthors' );
+		if( $noAuthors === '' ) {
+			return true;
+		}
+
+	}
+
+	protected function doProcess() {
 
 		$list = new \BlueSpice\Authors\AuthorsList(
 			$this->out->getTitle(),
