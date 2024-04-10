ffa748e98b6e1d0e4d1f8bb6388fdcdff4f612c0
Mschlindwein <schlindwein@hallowelt.com>
2024-04-10 08:03:34 +0200

    Fix user real name for authors page
    ERM36761
    
    [4.4.x]
    
    Change-Id: I9204f89bef2f9e330789cf0bbf4e43a043b08baf
    (cherry picked from commit 78c0cb68052b51b4ce6812781232a8024d0127b6)

--- src/Data/PageAuthors/Record.php
+++ src/Data/PageAuthors/Record.php
@@ -6,4 +6,5 @@ class Record extends \MWStake\MediaWiki\Component\DataStore\Record {
 	public const USER_IMAGE_HTML = 'user_image_html';
 	public const USER_NAME = 'user_name';
 	public const AUTHOR_TYPE = 'author_type';
+	public const USER_REAL_NAME = 'user_real_name';
 }
