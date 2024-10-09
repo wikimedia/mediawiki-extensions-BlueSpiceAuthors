8ee3f1b21bf2f70aa24947f88e07b1192059cb50
hslater <hamish.slater@hallowelt.com>
2024-10-09 23:45:09 +1300

    Use this.pageName for consistent context
    ERM38809
    
    [REL1_39, REL1_39-4.5.x, master]
    
    Change-Id: I86f4cc933b65ad6122e11321da71df6df359d11e
    (cherry picked from commit cb98360add1a90e24b58adbec6e4c4edcc211a06)

--- resources/ui/page/bluespice.authors.AuthorsInformationPage.js
+++ resources/ui/page/bluespice.authors.AuthorsInformationPage.js
@@ -1,4 +1,4 @@
-(function( mw, $, bs, undefined ) {
+( ( mw, bs ) => {
 	bs.util.registerNamespace( 'bs.authors.info' );
 
 	bs.authors.info.AuthorsInformationPage = function AuthorsInformationPage( name, config ) {
@@ -6,7 +6,7 @@
 		bs.authors.info.AuthorsInformationPage.super.call( this, name, config );
 	};
 
-	OO.inheritClass( bs.authors.info.AuthorsInformationPage, StandardDialogs.ui.BasePage );
+	OO.inheritClass( bs.authors.info.AuthorsInformationPage, StandardDialogs.ui.BasePage ); // eslint-disable-line no-undef
 
 	bs.authors.info.AuthorsInformationPage.prototype.setupOutlineItem = function () {
 		bs.authors.info.AuthorsInformationPage.super.prototype.setupOutlineItem.apply( this, arguments );
@@ -20,31 +20,49 @@
 		return;
 	};
 
-	bs.authors.info.AuthorsInformationPage.prototype.onInfoPanelSelect = function () {
-		var me = this;
-		if ( me.authorGrid === null ){
-			mw.loader.using( [ 'ext.oOJSPlus.data', 'oojs-ui.styles.icons-user' ] ).done( function () {
-				bs.api.store.getData( 'pageauthors' ).done( function ( data ) {
-					me.authorGrid = new OOJSPlus.ui.data.GridWidget( {
-						columns: {
-							user_name: {
-								headerText: mw.message( 'bs-authors-info-dialog-grid-column-author' ).text(),
-								type: 'user',
-								showImage: true
-							},
-							author_type: {
-								headerText: mw.message( 'bs-authors-info-dialog-grid-column-type' ).text(),
-								type: "text"
-							}
-						},
-						data: data.results
-					} );
-					me.$element.append( me.authorGrid.$element );
-				} )
-		 } )
+	bs.authors.info.AuthorsInformationPage.prototype.onInfoPanelSelect = async function () {
+		if ( !this.authorGrid ) {
+			await mw.loader.using( [ 'ext.oOJSPlus.data', 'oojs-ui.styles.icons-user' ] );
+
+			let authorsData;
+
+			try {
+				const api = new mw.Api();
+				const response = await api.get( {
+					action: 'query',
+					titles: this.pageName,
+					prop: 'info'
+				} );
+
+				const pageId = Object.keys( response.query.pages )[ 0 ];
+
+				const data = await bs.api.store.getData( 'pageauthors', {
+					context: {
+						wgArticleId: pageId
+					}
+				} );
+
+				authorsData = data.results || [];
+			} catch ( error ) {}
+
+			this.authorGrid = new OOJSPlus.ui.data.GridWidget( {
+				columns: {
+					user_name: { // eslint-disable-line camelcase
+						headerText: mw.message( 'bs-authors-info-dialog-grid-column-author' ).text(),
+						type: 'user',
+						showImage: true
+					},
+					author_type: { // eslint-disable-line camelcase
+						headerText: mw.message( 'bs-authors-info-dialog-grid-column-type' ).text(),
+						type: 'text'
+					}
+				},
+				data: authorsData
+			} );
+			this.$element.append( this.authorGrid.$element );
 		}
-	}
+	};
 
-	registryPageInformation.register( 'authors_infos', bs.authors.info.AuthorsInformationPage );
+	registryPageInformation.register( 'authors_infos', bs.authors.info.AuthorsInformationPage ); // eslint-disable-line no-undef
 
-})( mediaWiki, jQuery, blueSpice );
+} )( mediaWiki, blueSpice );
