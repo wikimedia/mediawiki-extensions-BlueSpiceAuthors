64c863014e93c47136c5801c0a2ddc140ae9427e
hslater <hamish.slater@hallowelt.com>
2025-01-23 17:00:33 +0100

    Update IContextSource namespace
    ERM38444
    
    [REL1_43, master]

--- src/Data/PageAuthors/Reader.php
+++ src/Data/PageAuthors/Reader.php
@@ -2,8 +2,8 @@
 
 namespace BlueSpice\Authors\Data\PageAuthors;
 
-use IContextSource;
 use MediaWiki\Config\Config;
+use MediaWiki\Context\IContextSource;
 use MediaWiki\MediaWikiServices;
 use MWStake\MediaWiki\Component\DataStore\DatabaseReader;
 use MWStake\MediaWiki\Component\DataStore\ReaderParams;
